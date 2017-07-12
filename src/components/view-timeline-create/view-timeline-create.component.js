0541e0092088f93d0cc7cc6f7cae2a19e0cf8cbc
unknown <ga92yup@mytum.de>
2017-07-12 22:38:46 +0200

    Committing with debug messages

--- src/components/view-timeline-create/view-timeline-create.component.js
+++ src/components/view-timeline-create/view-timeline-create.component.js
@@ -120,11 +120,14 @@ class ViewTimelineCreateComponentController{
                 this.startYear = startdate[0];
                 this.startMonth = startdate[1];
                 this.startDay = startdate[2];
-                var enddate = this.event.end.split("-");
-                this.endYear = enddate[0];
-                this.endMonth = enddate[1];
-                this.endDay = enddate[2];
-
+                console.log("Edit event function, printing START date Day:" + this.startDay + " Month:" + this.startMonth + " Year:" + this.startYear);
+                if(this.event.end != undefined) {
+                    var enddate = this.event.end.split("-");
+                    this.endYear = enddate[0];
+                    this.endMonth = enddate[1];
+                    this.endDay = enddate[2];
+                    console.log("Edit event function, printing END date Day:" + this.endDay + " Month:" + this.endMonth + " Year:" + this.endYear);
+                }
            this.editInProgress = true;
        }
        else {
@@ -136,13 +139,14 @@ class ViewTimelineCreateComponentController{
         let eventsList = this.timeline.getSelection();
         let eventId = eventsList[0];
         let eventToAdd = this.manipulateEvent(eventId);
-        this.items.update(eventToAdd);
+        this.items.remove(eventId);
+        this.items.add(eventToAdd);
         this.timeline.fit();
         var foundIndex = this.dataModel.content.eventItem.findIndex(x => x.id == eventId);
         this.dataModel.content.eventItem[foundIndex] = eventToAdd;
         var arrayLength = this.dataModel.content.eventItem.length;
         for (var i = 0; i < arrayLength; i++) {
-            console.log(this.dataModel.content.eventItem[i].id, this.dataModel.content.eventItem[i].content);
+            console.log(this.dataModel.content.eventItem[i].id, this.dataModel.content.eventItem[i].end);
         }
         this.editInProgress = false;
         this.clearEvent();
@@ -168,17 +172,21 @@ class ViewTimelineCreateComponentController{
         let eventToAdd = {};
         this.event.end = this.concatenateDate("end");
         this.event.start = this.concatenateDate("start");
-        if(this.event.end === "" || this.event.end === undefined) {
+
+        if(this.event.end =="" || this.event.end === undefined) {
+            console.log("Not creating the end of event");
             eventToAdd = {
                 "id": eventId, "content": this.event.template(),
                 "start": this.event.start
             };
         } else {
+            console.log("Creating the end of event");
             eventToAdd = {
                 "id": eventId, "content": this.event.template(),
                 "start": this.event.start, "end": this.event.end
             };
         }
+        console.log("The start of the event " + eventToAdd.start);
         return eventToAdd;
     }
 
@@ -206,7 +214,7 @@ class ViewTimelineCreateComponentController{
         this.dataModel.content.eventItem = this.items.get();
         var arrayLength = this.dataModel.content.eventItem.length;
         for (var i = 0; i < arrayLength; i++) {
-            console.log(this.dataModel.content.eventItem[i].id, this.dataModel.content.eventItem[i].content);
+            console.log(this.dataModel.content.eventItem[i].id, this.dataModel.content.eventItem[i].content, this.dataModel.content.eventItem[i].end);
         }
         this.dataModel.name = this.timeline.name;
         this.dataModel.description = this.timeline.description;
@@ -235,18 +243,19 @@ class ViewTimelineCreateComponentController{
     concatenateDate(choice) {
         var date = "";
         if (choice === "end") {
-            date = this.endYear;
-            if (this.endMonth != undefined) {
+            if(this.endYear != undefined && this.endYear != "")
+                     date = this.endYear;
+            if (this.endMonth != undefined && this.endMonth != "") {
                 date = date + "-" + this.endMonth;
-                if (this.endDay != undefined) {
+                if (this.endDay != undefined && this.endDay != "") {
                     date = date + "-" + this.endDay;
                 }
             }
         } else if (choice === "start") {
             date = this.startYear;
-            if (this.startMonth != undefined) {
+            if (this.startMonth != undefined && this.startMonth != "") {
                 date = date + "-" + this.startMonth;
-                if (this.startDay != undefined) {
+                if (this.startDay != undefined && this.startDay != "") {
                     date = date + "-" + this.startDay;
                 }
             }
